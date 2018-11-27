a0239c7ed6d2036e9fb29a78bd5ee368c76cf64f
RyanThomas <ryan.thomas@bedegaming.com>
2018-11-27 10:21:10 +0000

    Change topic of About page to Blog

--- PersonalWebsite/PersonalWebsite/Controllers/HomeController.cs
+++ PersonalWebsite/PersonalWebsite/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace PersonalWebsite.Controllers
 
 		public IActionResult About()
 		{
-			ViewData["Message"] = "Your application description page.";
+			ViewData["Message"] = "Your blog page.";
 
 			return View();
 		}
